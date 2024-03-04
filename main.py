580edd86b740cc222a524d6ba222a4d2d829264d
DanielAcevedoVega <antonydany236@gmail.com>
2024-03-04 09:09:09 -0500

    Agregar filtros en los clientes

---
+++ main.py
@@ -0,0 +1,8 @@
+from tabulate import tabulate
+import modules.getClients as cliente
+
+#print(cliente.getAllClientName())
+#print(cliente.getOneClientCodigo(1))
+#print(cliente.getAllClientCreditCiudad(5000, "Humanes"))
+#print(tabulate(cliente.getAllClientName(), tablefmt="grid"))
+print(tabulate(cliente.getAllClientPaisRegionCiudad("Australia")))
--- modules/getClients.py
+++ modules/getClients.py
@@ -1,8 +1,40 @@
-from storage.cliente import clientes
+import storage.cliente as cli
 
+def getAllClientName():
+    clientName = list()
+    for val in cli.clientes:
+        codigoName = dict({
+            "codigo_cliente": val.get('codigo_cliente'),
+            "nombre_cliente": val.get('nombre_cliente')
+        })
+        clientName.append(codigoName)
+    return clientName
 
+def getOneClientCodigo(codigo):
+    for val in cli.clientes:
+        if(val.get('codigo_cliente') == codigo):
+            return{
+                "codigo_cliente": val.get('codigo_cliente'),
+                "nombre_cliente": val.get('nombre_cliente')
+            }
+            
+def getAllClientCreditCiudad(limiteCredit, ciudad):
+    clienteCredit = list()
+    for val in cli.clientes:
+        if(val.get('limite_credito') >= limiteCredit and val.get('ciudad') == ciudad):
+            clienteCredit.append(val)
+    return clienteCredit
 
-def searchName():
-    print(clientes)
-
-#searchName("busque esto plis")
+def getAllClientPaisRegionCiudad(pais, region=None, ciudad=None):
+    clientZone = list()
+    for val in cli.clientes:
+        if (val.get('pais') == pais):
+            
+            if((region != None or val.get('region') == region)):
+                clientZone.append(val)
+            elif(val.get('region') == None):
+                clientZone.append(val)
+            elif(val.get('ciudad')== None):
+                clientZone.append(val)
+    return clientZone
+            
