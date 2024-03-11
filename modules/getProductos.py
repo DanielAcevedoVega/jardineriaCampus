9c3752ebdadba8a8ab2afa75605b5ede64046e24
DanielAcevedoVega <antonydany236@gmail.com>
2024-03-10 20:33:57 -0500

    Agrege el menu en reporte de productos

--- modules/getProductos.py
+++ modules/getProductos.py
@@ -6,9 +6,24 @@ def getAllProductosGamaAromaticas():
         if (val.get("gama") == "Aromáticas") and (val.get("cantidad_en_stock") > 100):
             productoAromatica.sort(key=lambda x: x.get("precio_venta"), reverse=True) 
             productoAromatica.append({
-                "precio_venta": val.get("precio_venta"),
-                "gama": val.get("gama"),
-                "nombre": val.get("nombre")
+                "Precio de venta": val.get("precio_venta"),
+                "Gama": val.get("gama"),
+                "Nombre": val.get("nombre")
             })
     return productoAromatica
 
+def menu():
+    print("""
+
+______                      _             _       ______              _            _            
+| ___ \                    | |           | |      | ___ \            | |          | |           
+| |_/ /___ _ __   ___  _ __| |_ ___    __| | ___  | |_/ / __ ___   __| |_   _  ___| |_ ___  ___ 
+|    // _ \ '_ \ / _ \| '__| __/ _ \  / _` |/ _ \ |  __/ '__/ _ \ / _` | | | |/ __| __/ _ \/ __|
+| |\ \  __/ |_) | (_) | |  | ||  __/ | (_| |  __/ | |  | | | (_) | (_| | |_| | (__| || (_) \__ \\
+\_| \_\___| .__/ \___/|_|   \__\___|  \__,_|\___| \_|  |_|  \___/ \__,_|\__,_|\___|\__\___/|___/
+          | |                                                                                   
+          |_|                                                                                   
+
+          1. Obtener todos los prodcutos de la gama Aromaticas
+
+""")
