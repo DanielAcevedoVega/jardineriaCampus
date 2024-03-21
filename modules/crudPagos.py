843770186b07514fde7b6c4d4d16283441551443
DanielAcevedoVega <antonydany236@gmail.com>
2024-03-21 00:10:09 -0500

    Actualizar con eleccion en pagos

--- modules/crudPagos.py
+++ modules/crudPagos.py
@@ -115,7 +115,7 @@ def deletePago(id):
                 if vali.validacionSiNo(confirmacion):
                     if confirmacion == "s":
                         peticion = requests.delete(f"http://154.38.171.54:5006/pagos/{id}")
-                        if(peticion.status_code == 204):
+                        if(peticion.ok):
                             return[["messege", "Pago eliminado correctamente"]]
                         break
                     else:
@@ -136,45 +136,95 @@ def deletePago(id):
 def updatePago(id):
     data = getPagoCodigo(id)
     if(len(data)):
-        pago = dict()
-        pago["codigo_cliente"] = data["codigo_cliente"]
-        while True:
+        print("Pago Encontrado")
+        print(tabulate([data], headers="keys", tablefmt="github"))
+        data["codigo_cliente"] = data["codigo_cliente"]
+        continuarActualizar = True
+        while continuarActualizar:
             try:
-                if(not pago.get("forma_pago")):
-                    formaPago = input("Ingrese la forma de pago: ")
-                    if(vali.validacionNombre(formaPago) is not None):
-                        pago["forma_pago"] = formaPago
-                    else:
-                        raise Exception("La forma de pago no cumple con lo establecido")  
-                    
-                if(not pago.get("id_transaccion")):
-                    idTransaccion = input("Ingrese la id de la transaccion: ")
-                    if(vali.validaiconTransccion(idTransaccion) is not None):
-                        pago["id_transaccion"] = idTransaccion
-                    else:
-                        raise Exception("La id de la transaccion no cumple con lo establecido")
-                    
-                if(not pago.get("fecha_pago")):
-                    fechaPago = input("Ingrese la fecha de pago: ")
-                    if(vali.validacionFecha(fechaPago) is not None):
-                        pago["fecha_pago"] = fechaPago
-                    else:
-                        raise Exception("La fehca no cumple con lo establecido") 
 
-                if(not pago.get("total")):
-                    total = input("Ingrese el total del pago: ")
-                    if(vali.validacionNumerica(total) is not None):
-                        total = int(total)
-                        pago["total"] = total
-                        break
-                    else:
-                        raise Exception("El pago no cumple con lo establecido")   
+                print("""
+                        ¿Que dato deseas cambiar?
+                        
+                    1. Nombre 
+                    2. Primer apellido
+                    3. Segundo apellido
+                    4. Extension
+                    5. Email
+                    6. Codigo de la oficina
+                    7. Codigo jefe
+                    8. Puesto
                     
+                """)
+                opcion = input("\nSeleccione una de las opciones: ")
+                if(vali.validacionOpciones(opcion) is not None):
+                    opcion = int(opcion)
+                    if(opcion >= 0 and opcion <= 8):
+                        if(opcion == 1):
+                            while True:
+                                try:
+                                    formaPago = input("Ingrese la forma de pago: ")
+                                    if(vali.validacionNombre(formaPago) is not None):
+                                        data["forma_pago"] = formaPago
+                                        break
+                                    else:
+                                        raise Exception("La forma de pago no cumple con lo establecido")  
+                                except Exception as error:
+                                    print(error)
+                        
+                        if(opcion == 2):
+                            while True:
+                                try:
+                                    idTransaccion = input("Ingrese la id de la transaccion: ")
+                                    if(vali.validaiconTransccion(idTransaccion) is not None):
+                                        data["id_transaccion"] = idTransaccion
+                                        break
+                                    else:
+                                        raise Exception("La id de la transaccion no cumple con lo establecido")
+                                except Exception as error:
+                                    print(error)
+                        if(opcion == 3):
+                            while True:
+                                try:
+                                    fechaPago = input("Ingrese la fecha de pago: ")
+                                    if(vali.validacionFecha(fechaPago) is not None):
+                                        data["fecha_pago"] = fechaPago
+                                        break
+                                    else:
+                                        raise Exception("La fehca no cumple con lo establecido") 
+                                except Exception as error:
+                                    print(error)
+                        if(opcion == 4):
+                            while True:
+                                try:
+                                    total = input("Ingrese el total del pago: ")
+                                    if(vali.validacionNumerica(total) is not None):
+                                        total = int(total)
+                                        data["total"] = total
+                                        break
+                                    else:
+                                        raise Exception("El pago no cumple con lo establecido")   
+                                except Exception as error:
+                                    print(error)
+                        
+
+                        confirmacion = ""            
+                        while (confirmacion !=  "s" and confirmacion != "n"):
+                            confirmacion = input("Deseas cambiar mas datos?(s/n): ")
+                            if vali.validacionSiNo(confirmacion):
+                                if confirmacion == "n":
+                                    continuarActualizar = False
+                                    break
+                                else:
+                                    confirmacion == "s"
+                                    break
+                            else:
+                                print("La confirmacion no cumple con lo establecido por favor solo s/n")
             except Exception as error:
                 print(error)
         
         headers = {'Content-Type': 'application/json', 'charset': 'utf-8'}
-        peticion = requests.put(f"http://154.38.171.54:5006/pagos/{id}", headers=headers, data=json.dumps(pago))
+        peticion = requests.put(f"http://154.38.171.54:5006/pagos/{id}", headers=headers, data=json.dumps(data))
         res = peticion.json()
         return [res]
     else:
